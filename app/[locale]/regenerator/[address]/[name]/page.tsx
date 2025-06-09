067f551b4e2846153be73767beb6540a8adb13f6
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-06-09 09:56:26 -0300

    feat: create regenerator page and supporter page

---
+++ app/[locale]/regenerator/[address]/[name]/page.tsx
@@ -0,0 +1,39 @@
+import initTranslations from "@/app/i18n";
+import TranslationsProvider from "@/components/TranslationsProvider";
+import type { Metadata } from "next";
+
+const i18nNamespaces = ["regenerator-page"];
+
+type Props = {
+  params: Promise<{ locale: string; address: string; name: string; }>;
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const locale = (await params).locale;
+  const { t } = await initTranslations(locale, i18nNamespaces);
+
+  return {
+    title: t("seo-title"),
+    description: t("seo-description"),
+  };
+}
+
+export default async function SupporterPage({ params }: Props) {
+  const { address, locale, name } = await params;
+  const { t, resources } = await initTranslations(locale, i18nNamespaces);
+
+  return (
+    <TranslationsProvider
+      namespaces={i18nNamespaces}
+      locale={locale}
+      resources={resources}
+    >
+      <main>
+        <div className="container mx-auto px-5 lg:px-20">
+          {address}
+          {name}
+        </div>
+      </main>
+    </TranslationsProvider>
+  );
+}
