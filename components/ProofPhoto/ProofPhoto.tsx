bbc79261263e198cc83421d9acf3c1d9493220b1
Emanoel Augusto <emanoelaugusto7@gmail.com>
2025-06-11 08:39:49 -0300

    fix: display proof photo

--- components/ProofPhoto/ProofPhoto.tsx
+++ components/ProofPhoto/ProofPhoto.tsx
@@ -1,28 +1,30 @@
-import { Jazzicon } from '@ukstv/jazzicon-react';
-import Image from 'next/image';
+import { Jazzicon } from "@ukstv/jazzicon-react";
+import Image from "next/image";
 
 interface Props {
-  address: string
+  address: string;
   hash?: string;
   size?: number;
 }
 
-export function ProofPhoto({ hash, size = 50 , address }: Props) {
-  
-  return(
-    <div className="relative rounded-full overflow-hidden border-2 border-black" style={{ width: size, height: size }}>
-      <Jazzicon
-        address={address}
-        className={`w-full h-full rounded-full`}
-      />
-
-      {hash && (
+export function ProofPhoto({ hash, size = 50, address }: Props) {
+  return (
+    <div
+      className="relative rounded-full overflow-hidden border-2 border-black"
+      style={{ width: size, height: size }}
+    >
+      {hash ? (
         <Image
           alt="profile photo"
           src={`${process.env.NEXT_PUBLIC_IPFS_GATEWAY}/ipfs/${hash}`}
-          className="w-full h-full rounded-full object-cover absoulte"
+          className="w-full h-full rounded-full object-cover absoulte top-0 left-0 z-10"
           width={200}
           height={200}
+        />
+      ) : (
+        <Jazzicon
+          address={address}
+          className={`w-full h-full rounded-full z-0`}
         />
       )}
     </div>
